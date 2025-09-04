e7285eca1e90f4c1c55461505e6540298d62b95e
Dexter2099 <164971293+Dexter2099@users.noreply.github.com>
2025-09-05 08:50:01 +1000

    Use local static file storage in tests

---
+++ config/test_settings.py
@@ -0,0 +1,22 @@
+"""Test-specific Django settings."""
+
+import os
+
+# Mark as build phase so base settings don't require S3 configuration.
+os.environ.setdefault("DJANGO_COLLECTSTATIC", "1")
+
+from .settings import *  # noqa: F401,F403
+
+
+# Use local storage backends during tests to avoid collectstatic and external services.
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
+
+STORAGES = {
+    **STORAGES,
+    "staticfiles": {"BACKEND": STATICFILES_STORAGE},
+    "default": {"BACKEND": DEFAULT_FILE_STORAGE},
+}
+
+MEDIA_ROOT = BASE_DIR / "test-media"
+
