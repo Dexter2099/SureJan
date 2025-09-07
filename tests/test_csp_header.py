f920c39cc7d2a2ac5076fecbcef088da3fd1fcf2
Dexter2099 <164971293+Dexter2099@users.noreply.github.com>
2025-09-08 06:10:55 +1000

    chore: remove thumbnail and provider settings

--- tests/test_csp_header.py
+++ tests/test_csp_header.py
@@ -1,44 +1,6 @@
-import pytest
 from django.test import override_settings
 
 from config import settings as conf_settings
-
-
-def _build_csp(providers):
-    img_src = ["'self'"]
-    for p in providers.values():
-        img_src.extend(p["img_hosts"])
-    img_src.append("data:")
-    return {"DIRECTIVES": {"img-src": tuple(img_src)}}
-
-
-@pytest.mark.parametrize(
-    "provider,expected_hosts",
-    [
-        ("YOUTUBE", ["https://*.ytimg.com"]),
-        ("X", ["https://*.twimg.com", "https://pbs.twimg.com"]),
-        (
-            "RUMBLE",
-            [
-                "https://rumblecdn.com",
-                "https://*.rumblecdn.com",
-                "https://i.rmbl.ws",
-                "https://*.rmbl.ws",
-            ],
-        ),
-    ],
-)
-def test_csp_header_includes_provider_hosts(client, provider, expected_hosts):
-    providers = {provider: conf_settings.EMBED_PROVIDERS[provider]}
-    assert providers[provider]["img_hosts"] == expected_hosts
-    csp = _build_csp(providers)
-    with override_settings(DEBUG=False, EMBED_PROVIDERS=providers, CONTENT_SECURITY_POLICY=csp):
-        resp = client.get("/healthz")
-    csp_header = resp["Content-Security-Policy"]
-    for host in expected_hosts:
-        assert host in csp_header
-    assert "'self'" in csp_header
-    assert "data:" in csp_header
 
 
 def test_csp_header_has_img_directive(client):
