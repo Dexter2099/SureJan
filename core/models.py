7bd6fc1036eda4c1daf18f01fe6702ad81ebc353
Dexter2099 <164971293+Dexter2099@users.noreply.github.com>
2025-09-09 07:43:31 +1000

    feat: safely restore post image fields

--- core/models.py
+++ core/models.py
@@ -60,6 +60,9 @@ class Post(models.Model):
     )
     link_domain = models.CharField(max_length=120, blank=True)
     image = models.ImageField(upload_to="posts/", null=True, blank=True)
+    image_thumb = models.ImageField(
+        upload_to="posts/thumbs/", null=True, blank=True
+    )
     score = models.IntegerField(default=0)
     hot_rank = models.FloatField(default=0, db_index=True)
     rising_rank = models.FloatField(default=0, db_index=True)
